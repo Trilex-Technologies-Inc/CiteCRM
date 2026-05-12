8961be08eb56a77e0abaeda4a7ee7a387408eef4
Marziye Momen <95857662+momen573@users.noreply.github.com>
2026-05-12 14:53:01 +0330

    Enhance validation checks for company edit form inputs; ensure isset checks for all variables before processing to prevent errors

--- modules/control/company_edit.php
+++ modules/control/company_edit.php
@@ -14,7 +14,7 @@ if(isset($VAR['submit'])) {
 
 
 	/* get pdf printing option */
-	if($VAR['pdf_print'] == 1) {
+	if(isset($VAR['pdf_print']) && $VAR['pdf_print'] == 1) {
 		$html_print = 0;
 		$pdf_print  = 1;
 	} else {
@@ -24,25 +24,25 @@ if(isset($VAR['submit'])) {
 
 $q = 'UPDATE '.PRFX.'SETUP SET ';
 
-if($VAR['parts_password'] !='') {
+if(isset($VAR['parts_password']) && $VAR['parts_password'] !='') {
 	$q .= 'PARTS_PASSWORD		= '. $db->qstr( md5($VAR['parts_password'])).', ';	
 }
 
-if($VAR['ups_password'] != '') {
+if(isset($VAR['ups_password']) && $VAR['ups_password'] != '') {
 	$q .= 'UPS_PASSWORD		= '. $db->qstr( $VAR['ups_password']		) .', ';
 }
 		$q .= '
 			HTML_PRINT 			= '. $db->qstr( $html_print          	) .',
 			PDF_PRINT				= '. $db->qstr( $pdf_print           	) .',
-			INVOCIE_TAX 			= '. $db->qstr( $VAR['inv_tax']      	) .',
-			INV_THANK_YOU 		= '. $db->qstr( $VAR['inv_thank_you'] 	) .',
-			WELCOME_NOTE			= '. $db->qstr( $VAR['welcome']      	).',
-			PARTS_LO				= '. $db->qstr( $VAR['parts_lo']			).',
-			SERVICE_CODE			= '. $db->qstr( $VAR['service_code']		).',
-			PARTS_MARKUP			= '. $db->qstr( $VAR['parts_markup'] 	).',
-			PARTS_LOGIN			= '. $db->qstr( $VAR['parts_login']		).',
-			UPS_LOGIN 			= '. $db->qstr( $VAR['ups_login']			).',
-			UPS_ACCESS_KEY		= '. $db->qstr( $VAR['ups_access_key']	);
+			INVOCIE_TAX 			= '. $db->qstr( isset($VAR['inv_tax']) && $VAR['inv_tax'] != '' ? $VAR['inv_tax'] : '0' ) .',
+			INV_THANK_YOU 		= '. $db->qstr( isset($VAR['inv_thank_you']) && $VAR['inv_thank_you'] != '' ? $VAR['inv_thank_you'] : ' ' ) .',
+			WELCOME_NOTE			= '. $db->qstr( isset($VAR['welcome']) && $VAR['welcome'] != '' ? $VAR['welcome'] : ' ' ) .',
+			PARTS_LO				= '. $db->qstr( isset($VAR['parts_lo']) && $VAR['parts_lo'] != '' ? $VAR['parts_lo'] : '0' ) .',
+			SERVICE_CODE			= '. $db->qstr( isset($VAR['service_code']) && $VAR['service_code'] != '' ? $VAR['service_code'] : ' ' ) .',
+			PARTS_MARKUP			= '. $db->qstr( isset($VAR['parts_markup']) && $VAR['parts_markup'] != '' ? $VAR['parts_markup'] : '0' ) .',
+			PARTS_LOGIN			= '. $db->qstr( isset($VAR['parts_login']) && $VAR['parts_login'] != '' ? $VAR['parts_login'] : ' ' ) .',
+			UPS_LOGIN 			= '. $db->qstr( isset($VAR['ups_login']) && $VAR['ups_login'] != '' ? $VAR['ups_login'] : ' ' ) .',
+			UPS_ACCESS_KEY		= '. $db->qstr( isset($VAR['ups_access_key']) && $VAR['ups_access_key'] != '' ? $VAR['ups_access_key'] : ' ' );
 
 	if(!$rs = $db->execute($q)) {
 		force_page('core', 'error&error_msg=MySQL Error: '.$db->ErrorMsg().'&menu=1&type=database');
@@ -57,15 +57,15 @@ unset($q);
 
 	/* update company information */
 	$q = 'UPDATE '.PRFX.'TABLE_COMPANY SET
-		  	COMPANY_NAME			= '. $db->qstr( $VAR['company_name']	) .',
-		  	COMPANY_ADDRESS 	= '. $db->qstr( $VAR['address']		) .',
-			COMPANY_CITY 		= '. $db->qstr( $VAR['city']			) .',
-			COMPANY_STATE		= '. $db->qstr( $VAR['state']			) .',
-			COMPANY_ZIP 			= '. $db->qstr( $VAR['zip']				) .',
-			COMPANY_COUNTRY		= '. $db->qstr( $VAR['country']).',
-			COMPNAY_PHONE		= '. $db->qstr( $VAR['phone']			) .',
-			COMPNAY_MOBILE		= '. $db->qstr( $VAR['mobile_phone']	) .', 
-			COMPANY_TOLL_FREE	= '. $db->qstr( $VAR['toll_free']		);
+		  	COMPANY_NAME			= '. $db->qstr( isset($VAR['company_name']) && $VAR['company_name'] != '' ? $VAR['company_name'] : ' ' ) .',
+		  	COMPANY_ADDRESS 	= '. $db->qstr( isset($VAR['address']) && $VAR['address'] != '' ? $VAR['address'] : ' ' ) .',
+			COMPANY_CITY 		= '. $db->qstr( isset($VAR['city']) && $VAR['city'] != '' ? $VAR['city'] : ' ' ) .',
+			COMPANY_STATE		= '. $db->qstr( isset($VAR['state']) && $VAR['state'] != '' ? $VAR['state'] : ' ' ) .',
+			COMPANY_ZIP 			= '. $db->qstr( isset($VAR['zip']) && $VAR['zip'] != '' ? $VAR['zip'] : ' ' ) .',
+			COMPANY_COUNTRY		= '. $db->qstr( isset($VAR['country']) && $VAR['country'] != '' ? $VAR['country'] : ' ' ) .',
+			COMPNAY_PHONE		= '. $db->qstr( isset($VAR['phone']) && $VAR['phone'] != '' ? $VAR['phone'] : ' ' ) .',
+			COMPNAY_MOBILE		= '. $db->qstr( isset($VAR['mobile_phone']) && $VAR['mobile_phone'] != '' ? $VAR['mobile_phone'] : ' ' ) .', 
+			COMPANY_TOLL_FREE	= '. $db->qstr( isset($VAR['toll_free']) && $VAR['toll_free'] != '' ? $VAR['toll_free'] : ' ' );
 		
 
 	
